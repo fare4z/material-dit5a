73f3cd8c063d608b6a07f81c5c20bc62d87cbb0c
Fareez Borhanudin <fare4z@gmail.com>
2026-03-05 09:06:35 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+include_once "header.php";
+
+?>
+
+<header class="bg-primary text-white text-center py-5 bg-gradient">
+        <div class="container">
+            <h1>Login</h1>
+            <p class="lead">Login Page</p>
+        </div>
+</header>
+
+    <section class="py-5">
+        <div class="container">
+            <div class="row text-center">
+                <form method="post">
+
+                </form
+               
+            </div>
+        </div>
+    </section>
+
+
+    <?php 
+    include_once "footer.php";
+    ?>
